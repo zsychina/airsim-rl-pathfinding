0cd9d434b2607ca66a1a9fe98f6beac60f70c043
zsychina <siyuan100@foxmail.com>
2024-01-07 01:49:45 +0800

    add api controller

---
+++ pygame_controller.py
@@ -0,0 +1,70 @@
+import airsim
+import pygame
+import time
+import sys
+
+pygame.init()
+display = pygame.display.set_mode((300, 300))
+
+client = airsim.MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
+
+client.takeoffAsync().join()
+
+# 基础的控制速度(m/s)
+vehicle_velocity = 2.0
+# 设置临时加速比例
+speedup_ratio = 10.0
+# 用来设置临时加速
+speedup_flag = False
+
+# 基础的偏航速率
+vehicle_yaw_rate = 5.0
+
+while True:
+    yaw_rate = 0.0
+    velocity_x = 0.0
+    velocity_y = 0.0
+    velocity_z = 0.0
+
+    time.sleep(0.02)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    scan_wrapper = pygame.key.get_pressed()
+
+    # 按下空格键加速10倍
+    if scan_wrapper[pygame.K_SPACE]:
+        scale_ratio = speedup_ratio
+    else:
+        scale_ratio = speedup_ratio / speedup_ratio
+
+    # 根据 'A' 和 'D' 按键来设置偏航速率变量
+    if scan_wrapper[pygame.K_a] or scan_wrapper[pygame.K_d]:
+        yaw_rate = (scan_wrapper[pygame.K_d] - scan_wrapper[pygame.K_a]) * scale_ratio * vehicle_yaw_rate
+
+    # 根据 'UP' 和 'DOWN' 按键来设置pitch轴速度变量(NED坐标系，x为机头向前)
+    if scan_wrapper[pygame.K_UP] or scan_wrapper[pygame.K_DOWN]:
+        velocity_x = (scan_wrapper[pygame.K_UP] - scan_wrapper[pygame.K_DOWN]) * scale_ratio
+
+    # 根据 'LEFT' 和 'RIGHT' 按键来设置roll轴速度变量(NED坐标系，y为正右方)
+    if scan_wrapper[pygame.K_LEFT] or scan_wrapper[pygame.K_RIGHT]:
+        velocity_y = -(scan_wrapper[pygame.K_LEFT] - scan_wrapper[pygame.K_RIGHT]) * scale_ratio
+
+    # 根据 'W' 和 'S' 按键来设置z轴速度变量(NED坐标系，z轴向上为负)
+    if scan_wrapper[pygame.K_w] or scan_wrapper[pygame.K_s]:
+        velocity_z = -(scan_wrapper[pygame.K_w] - scan_wrapper[pygame.K_s]) * scale_ratio
+
+    # print(f": Expectation gesture: {velocity_x}, {velocity_y}, {velocity_z}, {yaw_rate}")
+
+    # 设置速度控制以及设置偏航控制
+    client.moveByVelocityBodyFrameAsync(vx=velocity_x, vy=velocity_y, vz=velocity_z, duration=0.02,
+                                                yaw_mode=airsim.YawMode(True, yaw_or_rate=yaw_rate))
+
+    if scan_wrapper[pygame.K_ESCAPE]:
+        pygame.quit()
+        sys.exit()
