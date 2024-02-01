002d09e0f5465618b4ed35fbf0e736399697c56c
zsychina <siyuan100@foxmail.com>
2024-02-02 02:48:02 +0800

    rewrite env

--- blocks1.0/environmet.py
+++ blocks1.0/environmet.py
@@ -1,22 +1,31 @@
 import airsim
 import numpy as np
+import time
+import config
 
-d_v = d_vx = d_vy = d_vz = 5.0
+clockspeed = 10
+timeslice = 1 / clockspeed
+floorZ = 2
+speed_limit = 0.2
+
+d_v = d_vx = d_vy = d_vz = 2.0
 class Env:
     def __init__(self):
         self.client = airsim.MultirotorClient()
         # should be random
-        self.target_loc = airsim.Vector3r(100, 0, -5)
+        self.target_loc = airsim.Vector3r(100, 0, -8)
         self.step_count = 0
     
     def reset(self):
         self.client.reset()
         self.step_count = 0
         # should be random
-        self.target_loc = airsim.Vector3r(100, 0, -5)
+        self.target_loc = airsim.Vector3r(100, 0, -8)
         self.client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(0, 0, -10)), False)
         self.client.enableApiControl(True)
         self.client.armDisarm(True)
+        
+        self.last_distance = np.linalg.norm([self.target_loc.x_val, self.target_loc.y_val])
         
         return self._cal_observation()       
         
@@ -52,48 +61,72 @@ class Env:
         except:
             print(action)
         
+        self.client.simPause(False)
         self.client.moveByVelocityBodyFrameAsync(vx=velocity[0], vy=velocity[1], vz=0, 
-                                                 duration=0.02, yaw_mode=airsim.YawMode(True))
+                                                 duration=timeslice, yaw_mode=airsim.YawMode(True))
+        landed = False
+        has_collided = False
+        collision_count = 0
+        start_time = time.time()
+        while time.time() - start_time < timeslice: # ~= sleep(timeslice)
+            quad_pos = self.client.getMultirotorState().kinematics_estimated.position
+            quad_vel = self.client.getMultirotorState().kinematics_estimated.linear_velocity
+            
+            collided = self.client.simGetCollisionInfo().has_collided
+            landed = (quad_vel.x_val == 0 and quad_vel.y_val == 0 and quad_vel.z_val == 0)
+            landed = landed or quad_pos.z_val > floorZ 
+            collision = collided or landed
+            if collision:
+                collision_count += 1
+            if collision_count > 10:
+                has_collided = True
+                break
+         
+        self.client.simPause(True)
+         
+        dead = has_collided    
         observation = self._cal_observation()
-        reward = self._cal_reward(observation)
-        done = self._is_done(observation, reward)
+        reward = self._cal_reward(observation, dead)
+        done = self._is_done(observation, reward) or dead
         
         return observation, reward, done
 
-
-    def _cal_reward(self, observation):
-        '''
-        + -1000 if crashed
-        + 100/ distance
-        + -1/ distance_sensor_min
-        '''
-        reward = 0
-        cur_loc = observation[3: 6]
-        target = np.array([self.target_loc.x_val, self.target_loc.y_val, self.target_loc.z_val])
-        distance = np.linalg.norm(target - cur_loc)
-        reward += 10000/distance
+    
+    def _cal_reward(self, observation, dead):
+        quad_vel = self.client.getMultirotorState().kinematics_estimated.linear_velocity
+        vel = np.array([quad_vel.x_val, quad_vel.y_val, quad_vel.z_val], dtype=np.float)
+        speed = np.linalg.norm(vel)
         
+        cur_loc = observation[3: 5]
+        target = np.array([self.target_loc.x_val, self.target_loc.y_val])
+        current_distance = np.linalg.norm(target - cur_loc)
         distance_sensors = observation[6: 9]
-        # reward += -1/distance_sensors.min()
         
-        state = self.client.getMultirotorState()
-        is_crashed = state.collision.has_collided
-        is_landed = state.landed_state
-        if is_crashed:
-            reward += -100000000
+        
+        reward = 0
+        if dead:
+            reward += config.reward['dead']
+        
+        if current_distance < self.last_distance:
+            reward += config.reward['forward']
+        else:
+            reward += config.reward['backward']
+        
+        if speed < speed_limit:
+            reward += config.reward['slow']
+        
+        if current_distance < 5:
+            reward += config.reward['goal']
             
+        self.last_distance = current_distance
         return reward
         
         
     def _is_done(self, observation, reward):
-        '''
-        too long
-        too close
-        '''
         cur_loc = observation[3: 6]
         target = np.array([self.target_loc.x_val, self.target_loc.y_val, self.target_loc.z_val])
         distance = np.linalg.norm(target - cur_loc)
-        if distance < 5 or reward < -1000 or self.step_count > 2000 or np.absolute(observation[5]) < 2:
+        if distance < 5 or reward < -10 or self.step_count > 2000 or np.absolute(observation[5]) < 2:
             return True
         
         return False        
